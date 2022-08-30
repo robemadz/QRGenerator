653a77ca6174b45ad95309afec8608952b4c6264
Roberto González Michelena <robemadz@gmail.com>
2022-08-30 17:34:02 +0200

    create: boilerplate and Tailwind config // add: html and basic styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['JetBrains Mono', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
