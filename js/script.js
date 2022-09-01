d7a146d963756dea88af281dfe40c83277f5347a
Roberto González Michelena <robemadz@gmail.com>
2022-09-01 15:56:09 +0200

    fixed: upgrade ux with real time validation using JS // todo: create instructions section above form, final colors, implement footer and publish all in netlify

--- js/script.js
+++ js/script.js
@@ -13,27 +13,27 @@ const onSubmit = (e) => {
   //Bring values of the inputs
   const urlValue = document.getElementById('url').value;
   const sizeValue = document.getElementById('size').value;
+  let pattern = url.pattern;
+  if (pattern || urlValue === '') {
+    let regex = new RegExp(pattern);
+    if (!regex.exec(urlValue)) {
+      return;
+    } else {
+      console.log(urlValue);
+      showSpinner();
 
-  if (urlValue === '') {
-    errorMsg.classList.remove('invisible');
-    setTimeout(() => {
-      errorMsg.classList.add('invisible');
-    }, 1400);
-    return;
-  } else {
-    showSpinner();
-
-    setTimeout(() => {
-      hideSpinner();
-
-      generateQrCode(urlValue, sizeValue);
       setTimeout(() => {
-        const saveUrl = qrCode.querySelector('img').src;
-        addDownloadBtn(saveUrl);
-      }, 50);
+        hideSpinner();
 
-      form.reset();
-    }, 1400);
+        generateQrCode(urlValue, sizeValue);
+        setTimeout(() => {
+          const saveUrl = qrCode.querySelector('img').src;
+          addDownloadBtn(saveUrl);
+        }, 50);
+
+        form.reset();
+      }, 1400);
+    }
   }
 };
 
@@ -57,7 +57,7 @@ const addDownloadBtn = (saveUrl) => {
   const downloadLink = document.createElement('a');
   downloadLink.id = 'saveQrLink';
   downloadLink.classList =
-    'bg-lime-500 hover:bg-lime-400 text-white font-bold py-2 px-4 rounded-md w-1/2 mx-auto my-2';
+    'bg-lime-500 hover:bg-lime-400 text-white font-bold py-2 px-4 rounded-md w-1/2 mx-auto mt-2';
   downloadLink.href = saveUrl;
   downloadLink.download = 'qrCode';
   downloadLink.innerHTML = 'Descarga tu código QR';
@@ -71,5 +71,19 @@ const showSpinner = () => {
 const hideSpinner = () => {
   spinner.classList.add('hidden');
 };
+
+document.addEventListener('keyup', (e) => {
+  if (e.target.matches('#qrForm [required]')) {
+    let input = e.target;
+    let pattern = input.pattern;
+
+    if (pattern || input.value === '') {
+      let regex = new RegExp(pattern);
+      return !regex.exec(input.value)
+        ? errorMsg.classList.remove('invisible')
+        : errorMsg.classList.add('invisible');
+    }
+  }
+});
 
 form.addEventListener('submit', onSubmit);
