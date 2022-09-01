902c997b91c9721a7a91c94f1789191eee450dc7
Roberto González Michelena <robemadz@gmail.com>
2022-09-01 14:37:38 +0200

    feature: added download button for qr img

--- js/script.js
+++ js/script.js
@@ -21,15 +21,18 @@ const onSubmit = (e) => {
     }, 1400);
     return;
   } else {
-    url.classList.add('border-green-700');
     showSpinner();
 
     setTimeout(() => {
       hideSpinner();
 
       generateQrCode(urlValue, sizeValue);
+      setTimeout(() => {
+        const saveUrl = qrCode.querySelector('img').src;
+        addDownloadBtn(saveUrl);
+      }, 50);
+
       form.reset();
-      url.classList.remove('border-green-700');
     }, 1400);
   }
 };
@@ -43,7 +46,22 @@ const generateQrCode = (urlValue, sizeValue) => {
 };
 
 const cleanUI = () => {
+  const saveBtn = document.getElementById('saveQrLink');
   qrCode.innerHTML = '';
+  if (saveBtn) {
+    saveBtn.remove();
+  }
+};
+
+const addDownloadBtn = (saveUrl) => {
+  const downloadLink = document.createElement('a');
+  downloadLink.id = 'saveQrLink';
+  downloadLink.classList =
+    'bg-lime-500 hover:bg-lime-400 text-white font-bold py-2 px-4 rounded-md w-1/2 mx-auto my-2';
+  downloadLink.href = saveUrl;
+  downloadLink.download = 'qrCode';
+  downloadLink.innerHTML = 'Descarga tu código QR';
+  document.getElementById('generated').appendChild(downloadLink);
 };
 
 const showSpinner = () => {
