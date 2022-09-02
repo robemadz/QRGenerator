4cb116f956fda0e2167de842a71e8e7307e43950
Roberto González Michelena <robemadz@gmail.com>
2022-09-02 12:01:08 +0200

    refactor details

--- js/script.js
+++ js/script.js
@@ -3,25 +3,27 @@ const qrCode = document.getElementById('qrCode');
 const url = document.getElementById('url');
 const errorMsg = document.getElementById('errorMsg');
 const spinner = document.getElementById('spinner');
+const pattern = url.pattern;
 
 const onSubmit = (e) => {
   e.preventDefault();
 
-  //Clean the last QR code
+  //Clean the last QR code and download button associated
   cleanUI();
 
   //Bring values of the inputs
   const urlValue = document.getElementById('url').value;
   const sizeValue = document.getElementById('size').value;
-  let pattern = url.pattern;
+
+  //Validation at submit
   if (pattern || urlValue === '') {
     let regex = new RegExp(pattern);
     if (!regex.exec(urlValue)) {
       return;
     } else {
-      console.log(urlValue);
       showSpinner();
 
+      //After 1.4sec, hideSpinner, generate QR and another setTimout nested to add download btn that needs the QR src attribute
       setTimeout(() => {
         hideSpinner();
 
@@ -31,12 +33,14 @@ const onSubmit = (e) => {
           addDownloadBtn(saveUrl);
         }, 50);
 
+        //Reset form for UX purposes
         form.reset();
       }, 1400);
     }
   }
 };
 
+//Generate QR function
 const generateQrCode = (urlValue, sizeValue) => {
   const qrCode = new QRCode('qrCode', {
     text: urlValue,
@@ -45,6 +49,7 @@ const generateQrCode = (urlValue, sizeValue) => {
   });
 };
 
+//Clean user interface function
 const cleanUI = () => {
   const saveBtn = document.getElementById('saveQrLink');
   qrCode.innerHTML = '';
@@ -53,29 +58,32 @@ const cleanUI = () => {
   }
 };
 
+//generate download btn function with JS
 const addDownloadBtn = (saveUrl) => {
   const downloadLink = document.createElement('a');
   downloadLink.id = 'saveQrLink';
   downloadLink.classList =
-    'bg-lime-500 hover:bg-lime-400 text-white font-bold py-2 px-4 rounded-md w-1/2 mx-auto mt-2';
+    'bg-lime-500 hover:bg-lime-400 text-white font-bold py-2 px-4 rounded-md w-1/2 mx-auto mt-2 shadow-md';
   downloadLink.href = saveUrl;
   downloadLink.download = 'qrCode';
   downloadLink.innerHTML = 'Descarga tu código QR';
   document.getElementById('generated').appendChild(downloadLink);
 };
 
+//Function to show spinner
 const showSpinner = () => {
   spinner.classList.remove('hidden');
 };
 
+//Function to hide spinner
 const hideSpinner = () => {
   spinner.classList.add('hidden');
 };
 
+//Real time validation using custom pattern with regEx
 document.addEventListener('keyup', (e) => {
   if (e.target.matches('#qrForm [required]')) {
     let input = e.target;
-    let pattern = input.pattern;
 
     if (pattern || input.value === '') {
       let regex = new RegExp(pattern);
@@ -86,4 +94,5 @@ document.addEventListener('keyup', (e) => {
   }
 });
 
+//submit listener
 form.addEventListener('submit', onSubmit);
